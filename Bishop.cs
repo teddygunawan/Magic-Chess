b62af131341810d0e7b25c2c12f434c90d7cb4f2
teddygunawan <teddygunawan55@gmail.com>
2017-05-18 11:46:56 +0800

    Added Pieces and Board

---
+++ Bishop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SwinGameSDK;
+namespace MyGame
+{
+	public class Bishop: Piece
+	{
+		public Bishop (Color clr, Cell cl): base(clr, cl)
+		{
+
+		}
+
+		public override void Draw ()
+		{
+
+		}
+
+		public override bool MoveRestriction (Point2D pt1, Point2D pt2)
+		{
+			return true;
+		}
+	}
+}
---
+++ Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SwinGameSDK;
+namespace MyGame
+{
+	public class Board
+	{
+		static Cell [,] _cell = new Cell [8, 8];
+		public Board ()
+		{
+			int x = 0, y = 0;
+			bool check = true;
+
+			for (int i = 0; i < 8; i++) {
+				for (int k = 0; k < 8; k++) {
+					if (i % 2 == 0) {
+						if (check) {
+							_cell [i, k] = new Cell (Color.White, x, y);
+							check = false;
+						} else {
+							_cell [i, k] = new Cell (Color.DarkGray, x, y);
+							check = true;
+						}
+					} else {
+						if (check) {
+							_cell [i, k] = new Cell (Color.DarkGray, x, y);
+							check = false;
+						} else {
+							_cell [i, k] = new Cell (Color.White, x, y);
+							check = true;
+						}
+					}
+					x += 60;
+				}
+				x = 0;
+				y += 60;
+			}
+		}
+
+		public void Draw ()
+		{
+			foreach (Cell c in _cell)
+				c.Draw();
+		}
+	}
+}
